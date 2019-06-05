6a1e9670b96f8b8e59e63fd9e4154c6e24122434
Gautier Michelin <gm@ideesculture.com>
2019-06-05 14:58:04 +0200

    changing the way of controlling the type_id to be sure it’s a fabric, not an object

--- views/main_html.php
+++ views/main_html.php
@@ -28,39 +28,72 @@
  ?>
  <div class="dashboardWidgetContentContainer">
 <?php
+    $o_data = new Db();
  	$po_request			= $this->getVar('request');
  	if($this->request->user->get("user_id") != 1) {
+	 	
 	 	$fabrique = $this->request->user->get("sms_number");
-	 	require_once(__CA_LIB_DIR__."/ca/Search/ObjectSearch.php");
+	 	require_once(__CA_LIB_DIR__."/Search/ObjectSearch.php");
 	 	$o_search = new ObjectSearch(); //instantiate a new search object
-	 	$qr_hits = $o_search->search("idno:".$fabrique." AND ca_objects.type_id:23");
-	 	print "<script>console.log('Requête = idno:".$fabrique." AND ca_objects.type_id:23');</script>";
+	 	$qr_hits = $o_search->search("idno:".$fabrique);
+	 	print "<script>console.log('Requête = idno:".$fabrique."');</script>";
 	 	$count=0;
 		while($qr_hits->nextHit()) {
+			if ($qr_hits->get('ca_objects.type_id') != 23 ) continue;
 			print "<h3>Fabrique : ".$qr_hits->get('ca_objects.preferred_labels.name')." <a href=/gestion/index.php/editor/objects/ObjectEditor/Summary/object_id/".$qr_hits->get('ca_objects.object_id').">(".$qr_hits->get('ca_objects.idno').")</a></h3>";  //confirm the id number
 			print "<hr/>";
 			$object_id_fabrique =  $qr_hits->get('ca_objects.object_id');
-			$o_data = new Db();
-			 $qr_result = $o_data->query("SELECT ca_objects.object_id, idno, name FROM ca_objects LEFT JOIN ca_object_labels cal ON cal.object_id=ca_objects.object_id WHERE parent_id =".$object_id_fabrique." and deleted=0");
+			
+			//REQUETE EGLISES
+			 $qr_result = $o_data->query("SELECT ca_objects.object_id, idno, name, status FROM ca_objects LEFT JOIN ca_object_labels cal ON cal.object_id=ca_objects.object_id WHERE parent_id =".$object_id_fabrique." and deleted=0");
 			 
 			 while($qr_result->nextRow()) {
-			       ?>
-			      <form action="/gestion/index.php/editor/objects/ObjectEditor/Edit" method="post" id="NewChildForm" target="_top" enctype="multipart/form-data">
+?>
+                <form action="/gestion/index.php/editor/objects/ObjectEditor/Edit" method="post" id="NewChildForm" target="_top" enctype="multipart/form-data">
 				<input type="hidden" name="_formName" value="NewChildForm">
 				<input name="form_timestamp" value="1491396998" type="hidden">
 				<input name="type_id" value="27" type="hidden"><!-- objet physique -->
 				<input name="object_id" value="0" type="hidden">
 				<input name="parent_id" value="<?php print $qr_result->get('object_id'); ?>" type="hidden">
-				<p><?php print "Eglise <b><a href=http://acf.lescollections.be/gestion/index.php/find/SearchObjects/Index/search/".$qr_result->get('idno')."> ".$qr_result->get('name')." ; <small>".$qr_result->get('idno')."</small></b></a>"; ?>
-				<button type="submit">Ajouter un objet</button></P>
-				</form>
-
-			      <?php
-			      
-			 }
- 
- 
+                <?php print "Eglise <b><a href=".__CA_URL_ROOT__."/index.php/find/SearchObjects/Index/search/".$qr_result->get('idno')."> ".$qr_result->get('name')." ; <small>".$qr_result->get('idno')."</small></b></a> "; ?>
+<?php
+	
+			    $qr_result1 = $o_data->query("select avancement from acf_suivi_avancement where eglise_id = ".$qr_result->get('object_id'));
+              // print "</br>test avancement:";
+             //   $testeglise = new ca_objects($qr_result->get('object_id'));
+                
+              //  print "</br> ------- </br>";
+                $avancement = $qr_result1->getAllRows();
+                $avancement = reset($avancement)["avancement"]*1;
+?>
+                    <br/>
+                    <button type="submit">Ajouter un objet</button></form>
+                 <span style="float:right;padding-right:12px;">
+<?php
+switch($qr_result->get('status')) {
+    case 2:
+        print "<small style='color:gray;'>DEMANDE DE VALIDATION TRANSMISE AU DIOCÈSE</small>";
+        break;
+    case 3:
+        print "<small style='color:gray;'>INVENTAIRE VALIDÉ</small>";
+        break;
+    case 1:
+    case 0:
+    default:
+?>
+                <small style="color:gray;">RÉCOLEMENT IRPA</small>
+                <?php print " <a style='background-color:#53B1C5;color:white;border-radius:4px;padding:3px 4px;' href='".__CA_URL_ROOT__."/index.php/suiviInventaireEglises/Statistics/Eglise/ID/".$qr_result->get('object_id')."'>".(real) $avancement." %</a>"; ?>
+                <a style="border:1px solid #ccc;border-radius:4px;padding:2px 5px;color:#333;background-color: white" href="<?php print __CA_URL_ROOT__."/index.php/suiviInventaireEglises/Statistics/JaiFini/ID/".$qr_result->get('object_id'); ?>">J'ai fini</a>
+<?php
+        break;
+}
+?>
+                </span>
+                </P>
 				
+
+<?php
+			 }
 		}
 	} else {
 		print "Utilisateur admin logué, impossible d'afficher ici toutes les fabriques de toutes les églises.";
